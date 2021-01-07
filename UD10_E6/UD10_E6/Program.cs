7ca216aa9020dd7407a49993af63800d19a0e98d
ArnauDiezSans <76475912+ArnauDiezSans@users.noreply.github.com>
2021-01-07 16:01:06 +0100

    commit
    commit

---
+++ UD10_E6/UD10_E6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD10_E6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Libro L1 = new Libro ( "9780544003415", "El Señor de los Anillos", "J.R.R. Tolkien", 1200 );
+            Libro L2 = new Libro("8478884459", "Harry Potter y la piedra filosofal", "J.K. Rowling",309);
+            L1.ToString();
+            L2.ToString();
+            if (L1._Num_pags > L2._Num_pags)
+            {
+                Console.WriteLine(L1._Titulo + " tiene más páginas.");
+            }
+            else
+            {
+                Console.WriteLine(L2._Titulo + " tiene más páginas.");
+            }
+        }
+    }
+}
